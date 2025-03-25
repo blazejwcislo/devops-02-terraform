f16de58333cc0c2b1dd941337f6136f77d28447b
Blazej Wcislo <67894017+blazejwcislo@users.noreply.github.com>
2025-03-25 20:55:11 +0100

    test

--- 1.1/main.tf
+++ 1.1/main.tf
@@ -5,8 +5,6 @@ provider "aws" {
 resource "aws_instance" "ubuntu_instance" {
   ami           = "ami-084568db4383264d4" 
   instance_type = "t2.micro"
-  subnet_id     = aws_subnet.public_subnet.id
-  security_groups = [aws_security_group.ubuntu_sg.name]
 
   tags = {
     Name = "Ubuntu-Instance"
@@ -16,7 +14,6 @@ resource "aws_instance" "ubuntu_instance" {
 resource "aws_instance" "amazon_linux_instance" {
   ami           = "ami-08b5b3a93ed654d19"
   instance_type = "t2.micro"
-  subnet_id     = aws_subnet.public_subnet.id
 
   tags = {
     Name = "AmazonLinux-Instance"
--- 1.7 EXTRA/main.tf
+++ 1.7 EXTRA/main.tf
@@ -2,6 +2,10 @@ provider "aws" {
   region = "us-east-1"
 }
 
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+}
+
 resource "aws_security_group" "ubuntu_sg" {
   vpc_id = aws_vpc.main.id
 
